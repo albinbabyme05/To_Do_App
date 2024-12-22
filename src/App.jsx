042632c7a68149e8ed23945efb7ca5fc1f06d143
Albin Baby <albinbabayme05@gmail.com>
2024-12-23 00:29:20 +0100

    to do status set

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }
 
   function handleToDos(){
-    setToDos([...toDos, toDo])
+    setToDos([...toDos, {id: Date.now(), text: toDo, status: false}])
   }
 
   return (
@@ -31,12 +31,20 @@ function App() {
         <i onClick={handleToDos} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        { toDos.map((value)=>{
+        { toDos.map((obj)=>{
           return( 
             <div className="todo">
               <div className="left">
-                <input type="checkbox" name="" id="" />
-                <p>{value}</p>
+                <input onChange={(e)=>{
+                  setToDos(toDos.filter(obj2=>{
+                    if(obj2.id===obj.id){
+                      obj2.status = e.target.checked;
+                    }
+                    return obj2;
+                  }))
+                }}
+                value={obj.status} type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
               </div>
               <div className="right">
                 <i className="fas fa-times"></i>
